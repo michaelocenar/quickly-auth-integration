ccb1abcb1f7515819828fbbc2a0688d024f3d24d
michaelocenar <affectuminflamma@gmail.com>
2023-07-22 01:44:16 -0600

    Create basic Login component structure

---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import axios from 'axios';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  return (
+    <form>
+      
+    </form>
+  );
+}
+
+export default Login;
