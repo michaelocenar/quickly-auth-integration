64db71251af05751513959e7da10f328d7542bbb
michaelocenar <affectuminflamma@gmail.com>
2023-07-22 17:37:52 -0600

    configure app.js with routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Login from './Login';
-import Signup from './Signup';
-import Profile from './Profile';
+import Login from 'src/components/Login.js';
+import Signup from 'src/components/Signup.js';
+import Profile from 'src/components/Signup.js';
 
-
-
-function App() {
+const App = () => {
   return (
-    <Login />
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/signup">Signup</Link></li>
+            <li><Link to="/profile">Profile</Link></li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
