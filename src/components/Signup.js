b293e13530e001c68747b41945b33f0571a6357d
michaelocenar <affectuminflamma@gmail.com>
2023-07-22 10:15:24 -0600

    Create basic structure for Signup component

---
+++ src/components/Signup.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import validator from 'validator';
+
+const Signup = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <form>
+  
+    </form>
+  );
+}
+
+export default Signup;
