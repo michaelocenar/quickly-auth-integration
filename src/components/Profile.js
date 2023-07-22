1a03fad492a00ef5102ad4c809c7c6aff32b08d7
michaelocenar <affectuminflamma@gmail.com>
2023-07-22 17:32:17 -0600

    add html to return statement of profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,25 +5,29 @@ const Profile = () => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const fetchUser = async => {
+    const fetchUser = async () => {
       try {
         const token = localStorage.getItem('token');
         const response = await axios.get('https://api-dev.quicklyinc.com/auth/user', {
           headers: { Authorization: `Bearer ${token}` }
-        })
-
+        });
+        setUser(response.data);
       } catch (error) {
-
+        console.error('Failed to fetch user', error);
+        alert('Failed to fetch user');
       }
-    }; 
+    };
 
+    fetchUser();
+  }, []);
 
-  })
-
+  if (!user) return <h1>401: Unauthorized</h1>;
 
   return (
     <div>
-
+      <h1>Welcome {user.first_name}</h1>
+      <p>Email: {user.email}</p>
+      <p>Company: {user.company.name}</p>
     </div>
   );
 }
