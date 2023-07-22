f23c704741f95bcde2f2d008278e8cbe932f532f
michaelocenar <affectuminflamma@gmail.com>
2023-07-22 17:29:01 -0600

    create fetchUser function for profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Profile = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async => {
+      try {
+        const token = localStorage.getItem('token');
+        const response = await axios.get('https://api-dev.quicklyinc.com/auth/user', {
+          headers: { Authorization: `Bearer ${token}` }
+        })
+
+      } catch (error) {
+
+      }
+    }; 
+
+
+  })
+
 
   return (
+    <div>
 
+    </div>
   );
 }
 
