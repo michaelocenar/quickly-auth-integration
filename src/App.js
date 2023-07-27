6feaedfc0b112cfcb391447f3972ea6c00a655d9
michaelocenar <affectuminflamma@gmail.com>
2023-07-27 12:19:33 +0100

    add bootstrap to the app

--- src/App.js
+++ src/App.js
@@ -4,25 +4,30 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './components/Login';
 import Profile from './components/Profile';
 import Signup from './components/Signup';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+
 
 const App = () => {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/signup">Signup</Link></li>
-            <li><Link to="/profile">Profile</Link></li>
-          </ul>
-        </nav>
+      <Container>
+        <div>
+          <nav>
+            <ul>
+              <li><Link to="/login">Login</Link></li>
+              <li><Link to="/signup">Signup</Link></li>
+              <li><Link to="/profile">Profile</Link></li>
+            </ul>
+          </nav>
 
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </div>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </div>
+      </Container>
     </Router>
   );
 }
