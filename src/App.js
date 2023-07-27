3887ac67cf5a8126b74c96374d7c2203548ee405
michaelocenar <affectuminflamma@gmail.com>
2023-07-27 12:38:24 +0100

    add bootstrap to html elements in app

--- src/App.js
+++ src/App.js
@@ -5,31 +5,43 @@ import Login from './components/Login';
 import Profile from './components/Profile';
 import Signup from './components/Signup';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 
 
 const App = () => {
   return (
     <Router>
       <Container>
-        <div>
-          <nav>
-            <ul>
-              <li><Link to="/login">Login</Link></li>
-              <li><Link to="/signup">Signup</Link></li>
-              <li><Link to="/profile">Profile</Link></li>
-            </ul>
-          </nav>
-
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <span className="navbar-brand mb-0 h1 custom-title">Quickly</span>
+        </nav>
+        <div className="d-flex justify-content-center align-items-center custom-container">
+          <div className="m-3">
+            <Link to="/login" className="custom-button">
+              <Button variant="outline-primary" size="lg">Login</Button>
+            </Link>
+          </div>
+          <div className="m-3">
+            <Link to="/signup" className="custom-button">
+              <Button variant="outline-secondary" size="lg">Signup</Button>
+            </Link>
+          </div>
+          <div className="m-3">
+            <Link to="/profile" className="custom-button">
+              <Button variant="outline-success" size="lg">Profile</Button>
+            </Link>
+          </div>
         </div>
+
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
       </Container>
     </Router>
   );
 }
 
 export default App;
+
